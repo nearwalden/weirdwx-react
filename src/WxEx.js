2c0ce0df1020e8ba0c4bc030371c82d782989d83
David Douglas <dd@ai.co>
2019-01-13 17:29:50 -0500

    cleanup

--- src/WxEx.js
+++ src/WxEx.js
@@ -83,6 +83,7 @@ class WxEx extends Component {
 			<Head />
 			<nav className="navbar navbar-expand-lg navbar-light bg-light">
 				<a className="navbar-brand" href="#">Navbar</a>
+				<a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>			
 				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
