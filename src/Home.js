16dd87f802f5a37d90ea5499c8f26a88129d8efc
David Douglas <nearwalden@gmail.com>
2019-01-27 22:21:12 -0500

    fixing forms

--- src/Home.js
+++ src/Home.js
@@ -26,37 +26,36 @@ class Home extends Component {
     return (
         <div className="Home">	
         	<div className="row">
-				<div className="form-group col-md-12">
-						<p>Enter your location below.</p>
-						<form href="#" onSubmit={this.handlePlace}>
-							<label htmlFor="place">Location</label>
-							<input id="place" name="place" type="text" />
-							<button>Submit location</button>
-					  	</form>		   
+				<form className="col-md-12" onSubmit={this.handlePlace}>
+					<div className="form-group">
+						<label for="place">Your location</label>
+						<input className="form-control"id="place" name="place" type="text" />
 					</div>
-				</div>
-				<hr/>
-				<div className="row">
-					<div className="form-group col-md-12">
-						<h3>Location</h3>
-					</div>
-				</div>
-
-				<div className="row">
-					<div className="form-group col-md-12">
-						<form href="#" onSubmit={this.handleLatLon}>
-							<label htmlFor="lat">Latitude</label>
-							<input id="lat" name="lat" type="text" />
-
-							<label htmlFor="lon">Longitude</label>
-							<input id="lon" name="lon" type="text" />
-
-							<button>Submit lat/lon</button>
-					  	</form>		   
-				</div>
-
-				<hr/>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</form>		   
 			</div>
+			<hr/>
+			<div className="row">
+				<div className="form-group col-md-12">
+					<h3>Location</h3>
+				</div>
+			</div>
+
+			<div className="row">
+				<div className="form-group col-md-12">
+					<form href="#" onSubmit={this.handleLatLon}>
+						<label htmlFor="lat">Latitude</label>
+						<input id="lat" name="lat" type="text" />
+
+						<label htmlFor="lon">Longitude</label>
+						<input id="lon" name="lon" type="text" />
+
+						<button>Submit lat/lon</button>
+					</form>	
+				</div>	   
+			</div>
+
+			<hr/>
 		</div>
 							
     );
